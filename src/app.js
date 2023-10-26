96a4e5d3ce7ae5f1dff4c42965a14f9573a4d994
maximjuarez <manueljuarez10@gmail.com>
2023-10-26 11:14:52 -0300

    raiz de proyecto

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path= require('path')
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+ });
+
+ app.get('/carrito.html', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/views/carrito.html'));
+});
+
+app.get('/detallesproducto.html', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/views/detallesproducto.html'));
+});
+
+app.get('/login.html', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/registro.html', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/views/registro.html'));
+});
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Servidor en funcionamiento');
+//  });
+
+  app.listen(4545, () =>{
+    console.log('servidor escuchando')
+});
